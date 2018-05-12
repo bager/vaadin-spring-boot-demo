3e4f01c7f14ea6576f5454cb4da66be4e02f02ea
Blazej Palmaka <bager.dev@gmail.com>
2018-05-12 20:12:48 +0200

    init

---
+++ src/main/java/com/bager/vaadinspringbootdemo/Application.java
@@ -0,0 +1,12 @@
+package com.bager.vaadinspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
