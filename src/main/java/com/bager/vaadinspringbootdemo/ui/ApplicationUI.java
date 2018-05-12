ca92d8f448d76809165ce9dc2c21d2ba34a2fa5b
Blazej Palmaka <bager.dev@gmail.com>
2018-05-13 00:01:42 +0200

    add date field to page

--- src/main/java/com/bager/vaadinspringbootdemo/ui/ApplicationUI.java
+++ src/main/java/com/bager/vaadinspringbootdemo/ui/ApplicationUI.java
@@ -1,10 +1,13 @@
 package com.bager.vaadinspringbootdemo.ui;
 
+import java.time.LocalDate;
+
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.DateField;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Notification.Type;
@@ -15,7 +18,7 @@ import com.vaadin.ui.VerticalLayout;
 @SpringUI
 @Theme("valo")
 public class ApplicationUI extends UI {
-	
+
 	private VerticalLayout layout;
 
 	@Override
@@ -24,11 +27,15 @@ public class ApplicationUI extends UI {
 
 		layout.addComponent(new Label("Hello World!"));
 		layout.addComponent(new Button("Push Me!", click -> Notification.show("Pushed")));
-		
+
+		DateField dateField = new DateField("Current date", LocalDate.now());
+		layout.addComponent(dateField);
+
 		TextField nameInput = new TextField("Your name", "Blazej");
 		layout.addComponent(nameInput);
 		layout.addComponent(new Button("Push Me!", click -> {
-			Notification.show("Hello " + nameInput.getValue(), Type.WARNING_MESSAGE);
+			Notification.show("Hello " + nameInput.getValue(), "Today is " + dateField.getValue().toString(),
+					Type.WARNING_MESSAGE);
 		}));
 	}
 
